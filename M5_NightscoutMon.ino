d8a8c22ffe44312057cf166c659a1cc605b48f27
Ben West <bewest@gmail.com>
2021-02-26 15:39:26 -0800

    remove unused code

--- M5_NightscoutMon.ino
+++ M5_NightscoutMon.ino
@@ -674,34 +674,6 @@ void buttons_test() {
 }
 
 
-/*
-void listNetworks() {
-  // scan for nearby networks:
-  Serial.println("** Scan Networks **");
-  int numSsid = WiFi.scanNetworks();
-  if (numSsid == -1) {
-    Serial.println("Couldn't get a wifi connection");
-    while (true);
-  }
-
-  // print the list of networks seen:
-  Serial.print("number of available networks:");
-  Serial.println(numSsid);
-
-  // print the network number and name for each network found:
-  for (int thisNet = 0; thisNet < numSsid; thisNet++) {
-    Serial.print(thisNet);
-    Serial.print(") ");
-    Serial.print(WiFi.SSID(thisNet));
-    Serial.print("\tSignal: ");
-    Serial.print(WiFi.RSSI(thisNet));
-    Serial.print(" dBm");
-    Serial.print("\tEncryption: ");
-    printEncryptionType(WiFi.encryptionType(thisNet));
-  }
-}
-*/
-
 const char * CONSONANTS = "bcdfghjklmnpqrstvwxyz";
 const char * VOWELS = "aeiouy";
 int passLen = 8;
